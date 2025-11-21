f4669d140e090fe349f53aa0d1eb8d536d001d62
Nora Stella Winslow <gjerstadf@gmail.com>
2025-11-21 13:54:38 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import RestaurantList from "../components/RestaurantList";
+import AddRestaurantForm from "../components/AddRestaurantForm";
 
 export default function Home() {
   const [restaurants, setRestaurants] = useState([]);
@@ -11,9 +12,14 @@ export default function Home() {
       .catch((err) => console.error("API fetch error:", err));
   }, []);
 
+  const handleAdd = (newRestaurant) => {
+    setRestaurants((prev) => [...prev, newRestaurant]);
+  };
+
   return (
     <div>
       <h1>Restaurant Reviews</h1>
+      <AddRestaurantForm onAdd={handleAdd} />
       <RestaurantList restaurants={restaurants} />
     </div>
   );
