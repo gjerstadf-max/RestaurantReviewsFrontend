b52d2c28ca52020d5493184cf9dff84118dae600
gjerstadf-max <gjerstadf@gmail.com>
2025-11-23 14:48:08 -0800

    Fix Tailwind, clean project

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
-import React, { useEffect, useState } from "react";
-import RestaurantList from "../components/RestaurantList";
-import AddRestaurantForm from "../components/AddRestaurantForm";
-
 export default function Home() {
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/restaurants`)
-      .then((res) => res.json())
-      .then((data) => setRestaurants(data))
-      .catch((err) => console.error("API fetch error:", err));
-  }, []);
-
-  const handleAdd = (newRestaurant) => {
-    setRestaurants((prev) => [...prev, newRestaurant]);
-  };
-
   return (
-    <div>
-      <h1>Restaurant Reviews</h1>
-      <AddRestaurantForm onAdd={handleAdd} />
-      <RestaurantList restaurants={restaurants} />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working!
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./styles/**/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
